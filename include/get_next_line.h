b63cc6e35f15f2889761fb35e89fe4fa65025296
Manudies <manudies@gmail.com>
2025-05-22 14:01:44 +0200

    fix: norm42

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mdiestre <mdiestre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 12:24:44 by mdiestre          #+#    #+#             */
-/*   Updated: 2025/05/22 12:34:53 by mdiestre         ###   ########.fr       */
+/*   Updated: 2025/05/22 14:00:09 by mdiestre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,14 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
-
+//Main function
 char	*get_next_line(int fd);
+
+//Aux function
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
 
 #endif
